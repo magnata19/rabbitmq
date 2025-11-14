c8be6d1a78f5a197f5ff05b2963a7265779b057c
dave <dehpacifico@gmail.com>
2025-11-14 17:53:41 -0300

    Initial commit

---
+++ spring-rabbitmq/src/test/java/com/davidson/producerservice/SpringRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidson.producerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
